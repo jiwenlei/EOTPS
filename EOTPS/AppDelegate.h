73301f2ef2ed29b6eec204e4bb2b363e2e6efcb6
纪文磊 <jwl@jiwenleideMacBook-Air.local>
2015-07-15 00:37:54 +0800

    Initial Commit

---
+++ EOTPS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EOTPS
+//
+//  Created by 纪文磊 on 15/7/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EOTPS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EOTPS
+//
+//  Created by 纪文磊 on 15/7/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
